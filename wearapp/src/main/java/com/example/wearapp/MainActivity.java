473b56ed93a194926ac59d925e7404a6ce2bcc92
TommyTheHuman <tommyc98@yahoo.it>
2022-05-15 16:56:41 +0200

    implemented wear app with new intent service

---
+++ wearapp/src/main/java/com/example/wearapp/MainActivity.java
@@ -0,0 +1,300 @@
+package com.example.wearapp;
+
+import static android.content.Context.SENSOR_SERVICE;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Message;
+import android.os.PowerManager;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.FragmentActivity;
+import androidx.wear.ambient.AmbientModeSupport;
+
+import com.example.wearapp.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.android.gms.wearable.ChannelClient;
+import com.google.android.gms.wearable.MessageClient;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.WearableListenerService;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends FragmentActivity implements SensorEventListener, AmbientModeSupport.AmbientCallbackProvider {
+
+    private TextView mTextView;
+    private ActivityMainBinding binding;
+    private String TAG = "OROLOGIO";
+    private String smartphoneID = "b1887e3a";
+    private String communicationPath = "MyPadel";
+    private SensorManager sm;
+    private Sensor accelerometer;
+    private Sensor gyroscope;
+    private int SIZEOF_LONG = 8;
+    private int SIZEOF_FLOAT = 4;
+    private int SIZEOF_INT = 4;
+    private Button button;
+    //private List<SensedData> dataList = new ArrayList<>();
+    private DataList dataList = new DataList();
+    private final long FIVE_SECONDS_IN_NANOS = 5000000000L;
+    private AmbientModeSupport.AmbientController ambientController;
+    private int conta = 0;
+    private ChannelClient.Channel channel = null;
+    private OutputStream outputStream;
+    private String storagePath = "/storage/emulated/0/Android/data/com.example.wearApp/files";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Log.i(TAG, String.valueOf(getApplicationContext().getExternalFilesDir(null)));
+        sensorSetup();
+        setSmartphoneID();
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(view -> {
+            if("Start".contentEquals(button.getText())) {
+                registerSensorListener();
+
+                //start the mobile application by sending a message
+
+                Log.d(TAG, "entrato nel listener");
+
+                sendMsg("/hello-world-wear", "Start Activity");
+
+                button.setText(R.string.button_stop);
+            }
+            else {
+                unregisterSensorListener();
+                button.setText(R.string.button_start);
+                sendPacket();
+            }
+        });
+        //button.setEnabled(false);
+
+        //ambient mode
+        ambientController = AmbientModeSupport.attach(this);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        //
+        /*
+        //creazione canale
+        ChannelClient channelClient = Wearable.getChannelClient(getApplicationContext());
+        Task<ChannelClient.Channel> ch_task = channelClient.openChannel(smartphoneID, communicationPath);
+        ch_task.continueWithTask(new Continuation<ChannelClient.Channel, Task<OutputStream>>() {
+            @Override
+            public Task<OutputStream> then(@NonNull Task<ChannelClient.Channel> task) throws Exception {
+                channel = task.getResult();
+                return channelClient.getOutputStream(channel);
+            }
+        }).addOnSuccessListener(new OnSuccessListener<OutputStream>() {
+            @Override
+            public void onSuccess(OutputStream newOutputStream) {
+                Log.i(TAG, "successo");
+                outputStream = newOutputStream;
+                button.setEnabled(true);
+                Log.i(TAG, "Successo");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.i(TAG, "Fallimento: " + e.toString());
+            }
+        });
+        */
+    }
+
+
+    private void sendMsg( final String path, final String text ) {
+
+        Log.d(TAG, "PROVO A MANDARE");
+        setSmartphoneID();
+
+        Task<Integer> sendMessageTask = Wearable.getMessageClient(getApplicationContext()).sendMessage(smartphoneID, path, text.getBytes());
+
+        sendMessageTask.addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                Log.d(TAG, "Message Sent");
+            }
+        });
+    }
+
+
+
+    private void sensorSetup(){
+        sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        gyroscope = sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        Log.i(TAG, "accelerometer max FIFO: " + accelerometer.getFifoMaxEventCount());
+        Log.i(TAG, "gyroscope max FIFO: " + gyroscope.getFifoMaxEventCount());
+    }
+
+    private void registerSensorListener(){
+        sm.registerListener((SensorEventListener) this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        if(accelerometer == null) {
+            Log.d(TAG, "Accelerometer unavailable");
+            finish();
+        }
+        sm.registerListener((SensorEventListener) this, gyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+        if(gyroscope == null) {
+            Log.d(TAG, "Gyroscope unavailable");
+            finish();
+        }
+    }
+
+    private void unregisterSensorListener(){
+        sm.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        /*Log.i(TAG, String.valueOf(sensorEvent.timestamp) + ": ");
+        for (float f : sensorEvent.values)
+            Log.i(TAG, String.valueOf(f));*/
+        /*if(sensorEvent.sensor.getType() != Sensor.TYPE_GYROSCOPE && sensorEvent.sensor.getType() != Sensor.TYPE_ACCELEROMETER)
+            return;
+        dataList.addElement(new SensedData(sensorEvent));
+        if(dataList.getList().get(dataList.getList().size() - 1).timestamp - dataList.getList().get(0).timestamp > FIVE_SECONDS_IN_NANOS)
+            //Log.i(TAG, "invio " + dataList.getList().get(dataList.getList().size() - 1).timestamp + "|" + dataList.getList().get(0).timestamp);
+            sendPacket();
+
+         */
+    }
+
+    private void sendPacket() {
+        conta += dataList.getList().size();
+        Log.i(TAG, "number of sent packets = " + conta);
+        Log.i(TAG, "data: " + dataList.getList().get(0).timestamp + "|" + dataList.getList().get(0).values[1]);
+        TextView tv = (TextView) findViewById(R.id.counter);
+        tv.setText(String.valueOf(conta));
+        ByteBuffer tosend = createByteBuffer();
+        try {
+            outputStream.write(tosend.array());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        tosend.clear();
+        dataList.getList().clear();
+    }
+
+    private ByteBuffer createByteBuffer(){
+        ByteBuffer bb = ByteBuffer.allocate((SIZEOF_LONG + SIZEOF_FLOAT * 4) * dataList.getList().size());
+        for(SensedData sd: dataList.getList()){
+            if (sd.dataSource == 0)
+                bb.putFloat(0);
+            else if (sd.dataSource == 1)
+                bb.putFloat(1);
+            bb.putLong(sd.timestamp);
+            for (float val : sd.values)
+                bb.putFloat(val);
+        }
+        return bb;
+    }
+
+    private void writeToFile(String fileName, String content){
+        try {
+            File f = new File(storagePath, fileName);
+            FileWriter fw = new FileWriter(f, true);
+            fw.append(content);
+            fw.close();
+            // Toast fa vedere un pop up con scritto un messaggio
+            Toast.makeText(getApplicationContext(), "Wrote to file " + fileName, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        Log.i(TAG, "Accuracy changed");
+    }
+
+    private void setSmartphoneID(){
+        //trovare id dei nodi
+        Task<List<Node>> wearableList = Wearable.getNodeClient(getApplicationContext()).getConnectedNodes();
+        wearableList.addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                List<Node> list = task.getResult();
+                boolean trovato = false;
+                if(list.size() != 1){
+                    Log.i(TAG, "More than 1 node are connected");
+                    Log.d(TAG, "size: " + list.size());
+                    for(Node n: list) {
+                        Log.d(TAG, "nome: " + n.getDisplayName());
+                        if (!n.getDisplayName().contains("WATCH")) {
+                            smartphoneID = n.getId();
+                            trovato = true;
+                            Log.i(TAG, "Id of the smartphone obtained succesfully");
+                            break;
+                        }
+                    }
+                    if(!trovato) {
+                        Toast.makeText(getApplicationContext(), "More than 1 node are connected", Toast.LENGTH_SHORT).show();
+                        button.setEnabled(false);
+                        button.setText("ERROR");
+                    }
+                } else {
+                    Log.i(TAG, "Id of the smartphone obtained succesfully");
+                    smartphoneID = list.get(0).getId();
+                }
+            }
+        });
+    }
+
+    //ambient mode
+    @Override
+    protected void onPause(){
+        Log.i(TAG, "in pausa");
+        super.onPause();
+        unregisterSensorListener();
+    }
+
+    @Override
+    public AmbientModeSupport.AmbientCallback getAmbientCallback() {
+        return new MyAmbientCallback();
+    }
+
+    private class MyAmbientCallback extends AmbientModeSupport.AmbientCallback {
+        @Override
+        public void onEnterAmbient(Bundle ambientDetails) {
+            Log.i(TAG, "Enter ambient mode");
+            super.onEnterAmbient(ambientDetails);
+            button.getPaint().setAntiAlias(false);
+            //onExitAmbient();
+        }
+        @Override
+        public void onExitAmbient(){
+            Log.i(TAG, "Exit ambient mode");
+            super.onExitAmbient();
+            button.getPaint().setAntiAlias(true);
+        }
+    }
+    //
+}
---
+++ wearapp/src/main/java/com/example/wearapp/SensedData.java
@@ -0,0 +1,22 @@
+package com.example.wearapp;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+
+import java.io.Serializable;
+
+public class SensedData implements Serializable {
+    public float dataSource; //0 if the data come from the accelerometer, 1 if gyroscope
+    public long timestamp;
+    public float[] values; //sensed values on the three axys
+
+    SensedData(SensorEvent sensorEvent){
+        values = new float[3];
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            dataSource = 0;
+        else if (sensorEvent.sensor.getType() == Sensor.TYPE_GYROSCOPE)
+            dataSource = 1;
+        timestamp = sensorEvent.timestamp;
+        System.arraycopy(sensorEvent.values, 0, values, 0, 3);
+    }
+}
