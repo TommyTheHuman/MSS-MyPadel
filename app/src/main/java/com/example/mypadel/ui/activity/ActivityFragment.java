8bb5a4a4a639d560aa9d47d8553769af21703c00
Bicchie <i.bicchierini@studenti.unipi.it>
2022-05-19 10:16:46 +0200

    Change Icon, skeleton of Profile and Activity

--- app/src/main/java/com/example/mypadel/ui/activity/ActivityFragment.java
+++ app/src/main/java/com/example/mypadel/ui/activity/ActivityFragment.java
@@ -10,7 +10,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.Chronometer;
 
 import androidx.annotation.NonNull;
@@ -24,11 +23,7 @@ import com.example.mypadel.databinding.*;
 
 public class ActivityFragment extends Fragment {
 
-    private Button start_button;
     private Chronometer chronometer;
-    // variable to keep the state of activity -> false = not started, true = started
-    private Boolean activityState = false;
-    private final String fileNameProgress = "progressi.txt";
 
     private BroadcastReceiver broadcastReceiver;
     private String TAG = "ActivityFragment";
@@ -49,29 +44,7 @@ public class ActivityFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
-        start_button = (Button) getView().findViewById(R.id.start_button);
         chronometer = (Chronometer) getView().findViewById(R.id.chronometer);
-
-        start_button.setOnClickListener(new View.OnClickListener() {
-                                            @Override
-                                            public void onClick(View view) {
-                                                if(!activityState) {
-                                                    start_button.setText("STOP");
-                                                    activityState = true;
-                                                    chronometer.start();
-                                                } else {
-                                                    start_button.setText("START");
-                                                    activityState = false;
-                                                    // salva tutte cose in progressi
-                                                    // capisci come resettare
-                                                    // chronometer.setBase(0);
-                                                }
-
-                                                //DataCollection.startRecording();
-                                            }
-                                        }
-        );
-
     }
 
     @Override
@@ -80,35 +53,24 @@ public class ActivityFragment extends Fragment {
         binding = null;
     }
 
-    public void start_activity(){
-
-    }
-
     private void registerBroadcastReceiver(){
         broadcastReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                Log.i(TAG, "onReceive della broadcast");
                 if(intent.hasExtra("Chronometer")){
                     String value = intent.getStringExtra("Chronometer");
                     if(value.equals("start")){
                         doResetBaseTime();
-                        Log.i(TAG, "pre start");
                         chronometer.start();
-                        Log.i(TAG, "post start");
                     }else{
-                        Log.i(TAG, "dentro fragment");
                         chronometer.stop();
-                        Log.i(TAG, "cronometro stoppato");
                         sessionDuration = SystemClock.elapsedRealtime() - chronometer.getBase();
                         doResetBaseTime();
                         Log.i(TAG, String.valueOf(sessionDuration));
 
                         Intent intentClass = new Intent(context, StrokeClassification.class);
-                        Log.i(TAG, "intent creato");
                         intentClass.setAction("Classify");
                         intentClass.putExtra("Duration", sessionDuration);
-                        Log.i(TAG, "lancio servizio");
                         context.startService(intentClass);
 
                     }
@@ -122,8 +84,6 @@ public class ActivityFragment extends Fragment {
         // Returns milliseconds since system boot, including time spent in sleep.
         long elapsedRealtime = SystemClock.elapsedRealtime();
         // Set the time that the count-up timer is in reference to.
-        Log.i(TAG, "1");
         chronometer.setBase(elapsedRealtime);
-        Log.i(TAG, "2");
     }
 }
--- app/src/main/java/com/example/mypadel/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/mypadel/ui/profile/ProfileFragment.java
@@ -1,20 +1,49 @@
 package com.example.mypadel.ui.profile;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.example.mypadel.MainActivity;
+import com.example.mypadel.R;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.mypadel.databinding.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ProfileFragment extends Fragment {
 
+    private final String TAG = "Profile";
     private FragmentProfileBinding binding;
+    private TextView name;
+    private TextView height;
+    private TextView weight;
+    private Spinner spinner;
+
+    private String spinner_content;
+    private String name_content;
+    private String height_content;
+    private String weight_content;
+    private Context context;
+
+    private static String filePath;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,12 +53,108 @@ public class ProfileFragment extends Fragment {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        context = MainActivity.getContext();
+        filePath = context.getExternalFilesDir(null).toString();
+
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        MainActivity main = (MainActivity) getActivity();
+
+        Spinner dropdown = getView().findViewById(R.id.spinner);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"Beginner", "Intermediate", "Expert"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(main, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
+        spinner = (Spinner) getView().findViewById(R.id.spinner);
+        name = (TextView) getView().findViewById(R.id.name_filled);
+        height = (TextView) getView().findViewById(R.id.height_filled);
+        weight = (TextView) getView().findViewById(R.id.weight_filled);
+
+        readFromFile("profile.txt",name, spinner, height, weight);
+
+
+
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+
+        spinner = (Spinner) getView().findViewById(R.id.spinner);
+        spinner_content = spinner.getSelectedItem().toString();
+
+        name = (TextView) getView().findViewById(R.id.name_filled);
+        name_content = name.getText().toString();
+
+        height = (TextView) getView().findViewById(R.id.height_filled);
+        height_content = height.getText().toString();
+
+        weight = (TextView) getView().findViewById(R.id.weight_filled);
+        weight_content = weight.getText().toString();
+
+        Log.i(TAG, "Name:" + name_content + "\n Level:" + spinner_content
+              + "\n Height:" + height_content + "\n Weight:" + weight_content);
+
+        writeToFile("profile.txt", name_content, spinner_content, height_content, weight_content);
+
         binding = null;
     }
+
+
+    private void readFromFile(String fileName, TextView name, Spinner level, TextView height, TextView weight) {
+        File path = context.getExternalFilesDir(null);
+        File readFrom = new File(path, fileName);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(readFrom))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                if(!parts[0].equals("empty"))
+                    name.setText(parts[0]);
+                if(parts[1].equals("Beginner"))
+                    spinner.setSelection(0);
+                if(parts[1].equals("Intermediate"))
+                    spinner.setSelection(1);
+                if(parts[1].equals("Expert"))
+                    spinner.setSelection(2);
+                if(!parts[2].equals("empty"))
+                    height.setText(parts[2]);
+                if(!parts[3].equals("empty"))
+                    weight.setText(parts[3]);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeToFile(String fileName, String name, String level, String height, String weight){
+        try {
+            File f = new File(filePath, fileName);
+            FileWriter fw = new FileWriter(f, false);
+            if(name.equals(""))
+                name = "empty";
+            if(level.equals(""))
+                level = "empty";
+            if(height.equals(""))
+                height = "empty";
+            if(weight.equals(""))
+                weight = "empty";
+
+            String content = name + "," + level + "," + height + "," + weight;
+            fw.append(content);
+            fw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
